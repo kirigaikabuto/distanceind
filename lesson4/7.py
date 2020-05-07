38a64211c6211d8614e34d51949ad50a08de69f3
Your Name <tleugazy98.com>
2020-05-07 11:50:26 +0600

    dsd

--- lesson4/7.py
+++ lesson4/7.py
@@ -2,13 +2,14 @@
 # 1 2 3 4 
 # 5 6 7 8
 # 9 10 11 12
+import random
 n,m = input().split()#3 4
 n = int(n)
 m = int(m)
 arr=[]
 for i in range(n):
-    line = [int(i) for i in input().split()]
-    arr.append(line)
+    for j in range(m):
+        arr[i][j]=random.randint(0,20)
 sumi=0
 for i in range(n):
     for j in range(m):
