2393241252edc1132bb5228695315a206a86c066
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-05-12 18:27:04 +0600

    sdsd

---
+++ lesson9/main.py
@@ -0,0 +1,16 @@
+#Dog-(name,realage)
+#create class
+#class variable
+#object variable
+class Person:
+    #constructor
+    def __init__(self,n,a):
+        self.name = n
+        self.age = a
+
+
+
+#create object
+p1 = Person("kirito",98)
+print(p1.name)
+print(p1.age)
